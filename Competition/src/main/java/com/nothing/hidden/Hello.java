98afbc2c72a139df6a26db1f21c555501325d0f4
zuzo15@freemail.hu <Peter_Czuczay@EPHUBUDW0225.budapest.epam.com>
2014-01-10 15:03:57 +0100

    proba

--- Competition/src/main/java/com/nothing/hidden/Hello.java
+++ Competition/src/main/java/com/nothing/hidden/Hello.java
@@ -3,7 +3,7 @@ package com.nothing.hidden;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Ezio, so good to see You. How can I help You? - Leonardo");
+		System.out.println("Robot Ezio, so good to see You. How can I help You? - Leonardo");
 
 	}
 
